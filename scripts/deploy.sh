48117fb6c885c0f0b03bd58cfd8b5f217ee97fe8
dntjr8989 <dntjr8989@naver.com>
2022-06-06 11:35:54 +0900

    deploy.sh 롤백

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,31 +2,31 @@
 
 REPOSITORY=/home/ec2-user/app
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl photorage-member | grep java | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID" >> /home/ec2-user/deploy.log
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID" >> /home/ec2-user/deploy.log
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포" >> /home/ec2-user/deploy.log
+echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME" >> /home/ec2-user/deploy.log
+echo "> JAR NAME: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가" >> /home/ec2-user/deploy.log
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행" >> /home/ec2-user/deploy.log
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \ -Dspring.config.location=classpath:/application-test.yml \ -Dspring.profiles.active=test \ $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 # nohup실행 시 CodeDeploy는 무한 대기 합니다. 이 이슈를 해결하기 위해 nohub.out파일을 표준 입출력용으로 별도로 사용합니다. 이렇게 하지 않으면 nohup.out파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력됩니다. nohub이 끝나기 전까지 CodeDeploy도 끝나지 않으니 꼭 이렇게 해야합니다.
