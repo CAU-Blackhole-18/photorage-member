6a0f948a767caf292fe7afb4f33bd52571bf99ec
dntjr8989 <dntjr8989@naver.com>
2022-06-06 09:40:56 +0900

    testcode 삭제

--- src/test/java/cauBlackHole/photoragemember/PhotorageMemberApplicationTests.java
+++ src/test/java/cauBlackHole/photoragemember/PhotorageMemberApplicationTests.java
@@ -3,10 +3,10 @@ package cauBlackHole.photoragemember;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PhotorageMemberApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
