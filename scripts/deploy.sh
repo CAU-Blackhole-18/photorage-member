cefaa96ff40df5635f4aa73b0bd52498870205cb
dntjr8989 <dntjr8989@naver.com>
2022-06-06 18:51:15 +0900

    deploy.sh jar 실행부분 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,8 +28,5 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
- -Dspring.config.location=/home/ec2-user/app/application-test.yml \
- -Dspring.profiles.active=test \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=8080  -Dspring.profiles.active=test -Dspring.config.location=/home/ec2-user/app/application-test.yml $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 # nohup실행 시 CodeDeploy는 무한 대기 합니다. 이 이슈를 해결하기 위해 nohub.out파일을 표준 입출력용으로 별도로 사용합니다. 이렇게 하지 않으면 nohup.out파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력됩니다. nohub이 끝나기 전까지 CodeDeploy도 끝나지 않으니 꼭 이렇게 해야합니다.
